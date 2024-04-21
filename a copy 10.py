96ce3832dde50607afb1dfd9f1988ba8381446d0
aryanshygun <aryanshygun5272@gmail.com>
2024-04-21 22:20:00 +0330

    commit

--- a copy 10.py
+++ a copy 10.py
@@ -1,8 +1,13 @@
+from collections import Counter
+
+
 def comp(array1, array2):
     
     if array1 is None or array2 is None:
         return False
-
+    
+    if type(array1) != list 
+    
     xdict = {}
     for i in array1:
         xdict[i] = xdict.get(i, 0) + 1
@@ -12,9 +17,13 @@ def comp(array1, array2):
         x = i**(1/2)
         ydict[x] = ydict.get(x, 0) + 1
 
-    return xdict == ydict
-
-
+    if len(xdict) != len(ydict):
+        return False
+    
+    for i in xdict.keys():
+        if i not in ydict.keys() or xdict[i] != ydict[i]:
+            return False
+    return True
 
 
 
@@ -22,4 +31,3 @@ a1 = [121, 144, 19, 161, 19, 144, 19, 11]
 a2 = [11*21, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
 
 print(comp(a1, a2))
-
